2c203666dcc785fda3df30f0faa4503ee2fd58ef
motilosvan <motilosvan55@gmail.com>
2026-04-20 19:53:16 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,13 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.elitewalk_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.elitewalk_subnet.id
+}
+
+output "instance_ip" {
+  value = aws_instance.backend.public_ip
 }
 
 output "db_endpoint" {
